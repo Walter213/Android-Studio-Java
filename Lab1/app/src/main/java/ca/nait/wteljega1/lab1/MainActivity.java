0f2b51cc4b88ffa65829d1b45302f1e161998a1d
Walter213 <46505629+Walter213@users.noreply.github.com>
2020-01-22 11:43:35 -0700

    Started working on lab 1

---
+++ Lab1/app/src/main/java/ca/nait/wteljega1/lab1/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.nait.wteljega1.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
