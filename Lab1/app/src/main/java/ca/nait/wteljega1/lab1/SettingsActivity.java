4019cdf284c2d0a1c8312784600db84f25e9b972
Walter213 <46505629+Walter213@users.noreply.github.com>
2020-01-23 14:47:12 -0700

    Working more on lab 1 and worked with preferences

---
+++ Lab1/app/src/main/java/ca/nait/wteljega1/lab1/SettingsActivity.java
@@ -0,0 +1,16 @@
+package ca.nait.wteljega1.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
