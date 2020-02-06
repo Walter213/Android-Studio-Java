7026312ccd197e12751d64e2a92c1cb846ca8ce3
Walter213 <46505629+Walter213@users.noreply.github.com>
2020-02-06 14:56:17 -0700

    working on displaying spinner view

--- Lab1Reanimated/app/src/main/java/ca/nait/wteljega1/lab1reanimated/OscarCustomListActivity.java
+++ Lab1Reanimated/app/src/main/java/ca/nait/wteljega1/lab1reanimated/OscarCustomListActivity.java
@@ -2,7 +2,14 @@ package ca.nait.wteljega1.lab1reanimated;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
@@ -16,9 +23,10 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class OscarCustomListActivity extends ListActivity
+public class OscarCustomListActivity extends ListActivity implements AdapterView.OnItemSelectedListener
 {
     ArrayList<HashMap<String,String>> oscar = new ArrayList<HashMap<String,String>>();
+    public static String parameter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,13 +34,33 @@ public class OscarCustomListActivity extends ListActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oscar_custom_list);
 
+        Spinner categorySpinner = (Spinner)findViewById(R.id.category_spinner);
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(this, R.array.category_arrays, android.R.layout.simple_spinner_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(categoryAdapter);
+
+        categorySpinner.setOnItemSelectedListener(this);
+
         displayOscarReviews();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        parameter = parent.getItemAtPosition(position).toString().split("")[1].toLowerCase();
+        displayOscarReviews();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 
     private void displayOscarReviews()
     {
-        String[] keys = new String[] {"REVIEWER", "DATE", "NOMINEE"};
-        int[] ids = new int[] {R.id.custom_row_reviewer, R.id.custom_row_date, R.id.custom_row_nominee};
+        String[] keys = new String[] {"REVIEWER", "DATE", "CATEGORY", "NOMINEE", "REVIEW"};
+        int[] ids = new int[] {R.id.custom_row_reviewer, R.id.custom_row_date,R.id.custom_row_category, R.id.custom_row_nominee, R.id.custom_row_review};
 
         SimpleAdapter adapter = new SimpleAdapter(this, oscar, R.layout.activity_oscar_custom_list_row, keys, ids);
         populateList();
@@ -42,12 +70,13 @@ public class OscarCustomListActivity extends ListActivity
 
     private void populateList()
     {
-        BufferedReader in = null;
+        //BufferedReader in = null;
         try
         {
-            HttpClient client = new DefaultHttpClient();
+            Toast.makeText(this, "From populate " + parameter, Toast.LENGTH_LONG).show();
+           /* HttpClient client = new DefaultHttpClient();
             HttpGet request = new HttpGet();
-            request.setURI(new URI("http://www.youcode.ca/Lab01Servlet"));
+            request.setURI(new URI("http://www.youcode.ca/Lab01Servlet" + "?CATEGORY=" + category));
             HttpResponse response = client.execute(request);
             in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
@@ -56,16 +85,23 @@ public class OscarCustomListActivity extends ListActivity
             while((line = in.readLine()) != null)
             {
                 HashMap<String,String> temp = new HashMap<String,String> ();
-                temp.put("SENDER", line);
-
-                line = in.readLine();
-                temp.put("MESSAGE", line);
+                temp.put("REVIEWER", line);
 
                 line = in.readLine();
                 temp.put("DATE", line);
+
+                line = in.readLine();
+                temp.put("CATEGORY", line);
+
+                line = in.readLine();
+                temp.put("NOMINEE", line);
+
+                line = in.readLine();
+                temp.put("REVIEW", line);
+
                 oscar.add(temp);
             }
-            in.close();
+            in.close();*/
         }
         catch (Exception e)
         {
